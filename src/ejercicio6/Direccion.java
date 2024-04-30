6c68ff451ad7c33aa843dacd9db2c499d928ccbc
davidsuareztames <davidsuareztames@localhost>
2024-04-30 09:20:55 +0200

    Creacion de una Clase Direccion
    Con sus atributos: direccion, ciudad y codigoPostal y sus getters y
    setters

---
+++ src/ejercicio6/Direccion.java
@@ -0,0 +1,36 @@
+package ejercicio6;
+
+public class Direccion {
+
+	private String direccion;
+    private String ciudad;
+    private String codigoPostal;
+    
+    
+	public String getDireccion() {
+		return direccion;
+	}
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	
+	
+	public String getCiudad() {
+		return ciudad;
+	}
+	public void setCiudad(String ciudad) {
+		this.ciudad = ciudad;
+	}
+	
+	
+	public String getCodigoPostal() {
+		return codigoPostal;
+	}
+	public void setCodigoPostal(String codigoPostal) {
+		this.codigoPostal = codigoPostal;
+	}
+    
+    
+    
+
+}
--- src/ejercicio6/ShotgunSurgery.java
+++ src/ejercicio6/ShotgunSurgery.java
@@ -1,5 +1,46 @@
 package ejercicio6;
 
+import ejercicio6.ShotgunSurgery.Cliente;
+
+/**
+ * @Author David
+ */
 public class ShotgunSurgery {
 
+	
+	public class Cliente {
+		
+		
+	    private String nombre;
+	    private String apellido;
+	    private Direccion direccion;
+
+	    // Getters y setters
+
+	    
+	    /**
+	     * 
+	     * @param nombre del set del nombre
+	     */
+	    public void setNombre(String nombre) {
+	        this.nombre = nombre;
+	    }
+
+	    /**
+	     * 
+	     * @param apellido del set del apellido
+	     */
+	    public void setApellido(String apellido) {
+	        this.apellido = apellido;
+	    }
+
+	    
+
+	    
+	    
+		
+	    
+	    
+	    
+	}
 }
