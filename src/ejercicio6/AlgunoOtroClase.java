b5e74e314e013bb10b8d8eaa2aba29ff4de698d0
davidsuareztames <davidsuareztames@localhost>
2024-04-30 09:14:36 +0200

    Ejercicio refactorizado de ShotgunSurgery

---
+++ src/ejercicio6/AlgunoOtroClase.java
@@ -0,0 +1,13 @@
+package ejercicio6;
+
+import ejercicio6.ShotgunSurgery.Cliente;
+
+public class AlgunoOtroClase {
+
+	public void setDireccion(Cliente cliente, String nuevaDireccion) {
+		
+		cliente.setDireccion(nuevaDireccion);
+	        // Otro código aquí
+	}
+	
+}
---
+++ src/ejercicio6/OtraClaseMas.java
@@ -0,0 +1,12 @@
+package ejercicio6;
+
+import ejercicio6.ShotgunSurgery.Cliente;
+
+public class OtraClaseMas {
+
+	public void enviarCorreo(Cliente cliente) {
+		
+        String direccion = cliente.getDireccion();
+        // Lógica para enviar correo a la dirección
+    }
+}
